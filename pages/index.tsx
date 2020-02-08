f63681de98b5b77e771599ec846c7b20908a5a76
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-02-08 21:14:40 +0300

    style: rearrange imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Layout from "../components/layout";
 import { Text } from "@chakra-ui/core";
+
+import Layout from "../components/layout";
 
 const Home = () => (
   <Layout>
