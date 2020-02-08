2eecf4163cc3c5912f288efba568a77a163cc4d5
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-02-08 21:13:18 +0300

    feat: add index page

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/layout";
+import { Text } from "@chakra-ui/core";
+
+const Home = () => (
+  <Layout>
+    <Text fontSize="md">Hello, world!</Text>
+  </Layout>
+);
+
+export default Home;
