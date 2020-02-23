b04212945101cb9aed9b9005a45f87daa068beb5
Andrey Krasnov <34657799+Doesntmeananything@users.noreply.github.com>
2020-02-23 15:15:41 +0300

    feat: add image grid with placeholder sources (#6)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
 import React from "react";
-import { Text } from "@chakra-ui/core";
+import { Grid, Image, Flex } from "@chakra-ui/core";
 
 import { Layout } from "../components/layout";
 
+type GridImageProps = {
+  imageUrl: string;
+  key: number;
+};
+
+const GridImage = ({ imageUrl }: GridImageProps) => (
+  <Image
+    fallbackSrc="https://via.placeholder.com/293"
+    size="293px"
+    src={imageUrl}
+    alt="random image"
+  />
+);
+
 const Home = () => (
   <Layout>
-    <Text fontSize="md">Hello, world!</Text>
+    <Flex justify="center" mt={6}>
+      <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+        {[...new Array(9)].map((_, i) => (
+          <GridImage
+            key={i}
+            imageUrl={`https://picsum.photos/293?random=${i}.webp`}
+          />
+        ))}
+      </Grid>
+    </Flex>
   </Layout>
 );
 
